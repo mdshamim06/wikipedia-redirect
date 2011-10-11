4174972b748dc9c8ec164a663f5b41b1a32d7b9e
hideki.shima@gmail.com <hideki.shima@gmail.com@dc32e5c9-47de-8e86-b990-5e641a5b48a1>
2011-10-11 06:47:41 +0000

    added more examples

--- trunk/edu.cmu.lti.wikipedia_redirect/src/edu/cmu/lti/wikipedia_redirect/Demo.java
+++ trunk/edu.cmu.lti.wikipedia_redirect/src/edu/cmu/lti/wikipedia_redirect/Demo.java
@@ -4,11 +4,12 @@ import java.io.File;
 public class Demo {
   public static void main(String[] args) throws Exception {
     WikipediaRedirect wr = IOUtil.load(new File(args[0]));
-    String[] srcTerms = {"オサマビンラディン", "オサマ・ビンラーディン"
-            ,"慶大", "NACSIS", "平成12年", "3.14"};
+    String[] srcTerms = {"オサマビンラディン", "オサマ・ビンラーディン",
+            "東日本大地震","東日本太平洋沖地震" ,"慶大", "NACSIS", 
+            "ダイアモンド", "アボガド", "バイオリン", "平成12年", "3.14"};
     StringBuilder sb = new StringBuilder();
     for ( String src : srcTerms ) {
-      sb.append("\""+src +"\" is redirected to \""+wr.get(src)+"\"\n");
+      sb.append("\""+wr.get(src)+"\" was redirected from \""+src+"\"\n");
     }
     System.out.println(sb.toString());
   }
