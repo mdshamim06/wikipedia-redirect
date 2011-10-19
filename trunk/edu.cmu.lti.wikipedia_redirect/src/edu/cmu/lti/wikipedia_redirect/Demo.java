f0e0d6e880936969be34902f7e926aec59fced75
hideki.shima@gmail.com <hideki.shima@gmail.com@dc32e5c9-47de-8e86-b990-5e641a5b48a1>
2011-10-19 22:08:25 +0000

    incorporating wikipedia hypernym

--- trunk/edu.cmu.lti.wikipedia_redirect/src/edu/cmu/lti/wikipedia_redirect/Demo.java
+++ trunk/edu.cmu.lti.wikipedia_redirect/src/edu/cmu/lti/wikipedia_redirect/Demo.java
@@ -3,7 +3,7 @@ import java.io.File;
 
 public class Demo {
   public static void main(String[] args) throws Exception {
-    WikipediaRedirect wr = IOUtil.load(new File(args[0]));
+    WikipediaRedirect wr = IOUtil.loadWikipediaRedirect(new File(args[0]));
     String[] srcTerms = {"オサマビンラディン", "オサマ・ビンラーディン",
             "東日本大地震","東日本太平洋沖地震" ,"慶大", "NACSIS", 
             "ダイアモンド", "アボガド", "バイオリン", "平成12年", "3.14"};
